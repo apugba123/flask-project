51be8be244c6b7e4a29c5c1dd75e7e3b2139b074
ak <apugba16@gmail.com>
2017-07-14 09:49:21 +0000

    init

---
+++ myapp.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return "hello"
+
+@app.route('/whereami')
+def whereami():
+	return "Koforidua"
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0")
+
+
